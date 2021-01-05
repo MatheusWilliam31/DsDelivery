ce7ef2d678fed2d932a60234fdadff8128fbac9a
Matheusilliam31 <mwsl.loose@hotmail.com>
2021-01-05 09:52:08 -0300

    PUT /orders/id/delivered

--- back-end/src/main/java/com/devsuperior/dsdelivery/services/OrderService.java
+++ back-end/src/main/java/com/devsuperior/dsdelivery/services/OrderService.java
@@ -33,12 +33,20 @@ public class OrderService {
 
 	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
-				Instant.now(), OrderStatus.PENDING);
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
 		for (ProductDTO p : dto.getProducts()) {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
